22be13b25f684697f21a5fa0950568d3ea1fa21a
Emin <makhmudovemin441@gmail.com>
2024-11-04 21:31:04 +0500

    sign in handler 30% with extra mistakes formatting

--- handlers.py
+++ handlers.py
@@ -21,6 +21,36 @@ def main_menu(e1,e2,e3,btn1,btn2,lb1,lb2,lb3, window):
 def signin_handler(btn1, btn2, window):
    btn1.after(0, btn1.destroy)
    btn2.after(0, btn2.destroy) 
+   name_label = tk.Label(window, text='User name')
+   name_label.grid(row=3,column=3)
+   
+   pass_label = tk.Label(window, text='Password')
+   pass_label.grid(column=3,row=3,rowspan=2)
+   
+   secret_code = tk.Label(window, text='Secret code')
+   secret_code.grid(column=3, row=4)
+   
+   user_name = tk.Entry(window, width=50)
+   user_name.insert(0, 'Enter user name')
+   user_name.grid(column=4, row=3, rowspan=1)
+
+   
+   user_password = tk.Entry(window, width=50)
+   user_password.insert(0, 'Enter your password')
+   user_password.grid(column=4,row=3,rowspan=2)  
+   
+   secret_code_entry = tk.Entry(window, width=50)
+   secret_code_entry.insert(0, 'Enter secret code')
+   secret_code_entry.grid(column=4, row=4)
+   
+   
+   button_sub = tk.Button(window, text='Submit', command=lambda:signup_text_handling(user_name,user_password,secret_code_entry,back_button,button_sub,name_label,pass_label,secret_code,window))
+   button_sub.grid(row=5, column=4)
+   
+   back_button = tk.Button(window, text='Back', fg='blue' ,command=lambda:main_menu(user_name,secret_code_entry,
+                                                                         user_password,button_sub,back_button,
+                                                                         name_label,pass_label,secret_code, window))
+   back_button.grid(column=1, row=1)
    
     
 def signup_handler(btn1, btn2, window):
@@ -54,7 +84,7 @@ def signup_handler(btn1, btn2, window):
    button_sub = tk.Button(window, text='Submit', command=lambda:signup_text_handling(user_name,user_password,secret_code_entry,back_button,button_sub,name_label,pass_label,secret_code,window))
    button_sub.grid(row=5, column=4)
    
-   back_button = tk.Button(window, text='Back', command=lambda:main_menu(user_name,secret_code_entry,
+   back_button = tk.Button(window, text='Back', fg='blue',command=lambda:main_menu(user_name,secret_code_entry,
                                                                          user_password,button_sub,back_button,
                                                                          name_label,pass_label,secret_code, window))
    back_button.grid(column=1, row=1)
