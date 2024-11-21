d51ae0a9aa3e725a30486a7255506e76ac59e06f
Emin <makhmudovemin441@gmail.com>
2024-11-21 18:51:15 +0500

    1

--- passw_table.py
+++
@@ -1,9 +0,0 @@
-def table_creating(window):
-    from tkinter import ttk
-    import tkinter as tk
-    tree = ttk.Treeview(columns=("id", "app", "login", "password"), show="headings")
-    tree.heading("id",text="ID")
-    tree.heading("app", text="App")
-    tree.heading("login", text="Login")
-    tree.heading("password", text="Password")
-        
