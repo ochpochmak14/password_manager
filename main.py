508179ec6709710b51bb37391fd593bca35d2a54
Emin <makhmudovemin441@gmail.com>
2024-11-27 22:27:58 +0500

    last recommit

--- main.py
+++ main.py
@@ -5,13 +5,11 @@ from window_settings.window_settings import *
 def main():
     from db_operations.db_connect import db_connect
     from db_operations.create_db import db_create
-    from db_operations.passw_table import password_table_creating
     
     my_window = tk.Tk()
 
     con = db_connect()
     db_create(con)
-    password_table_creating(con)
     
     settings(my_window)
     f(my_window)
